ed237abeeee1593ffd0dd0c3913c84043cbee1f1
Denis Martynov <66518492+qah3set@users.noreply.github.com>
2022-08-24 14:18:42 +0300

    Add files via upload

---
+++ booking-telegram-bot/main.py
@@ -0,0 +1,21 @@
+from telegram import Update, ParseMode
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from Token import Token
+
+def start(update: Update, context: CallbackContext) -> None:
+    chat_id = str(update.message.chat_id)
+    if "api" not in context.user_data:
+        update.message.reply_text(
+            'Привет, я мастер по бровям Алина, чем я могу Вам помочь?')
+        return
+
+def main() -> None:
+    token = Token().get()
+    updater = Updater(token)
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
