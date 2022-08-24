5bf8f37167081905a8e9b52ecf07986a48a24110
Denis Martynov <66518492+qah3set@users.noreply.github.com>
2022-08-24 20:57:57 +0300

    fix untracked files

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
-from telegram import Update, ParseMode
-from telegram.ext import Updater, CommandHandler, CallbackContext
+import telebot
+from telebot import types
 from Token import Token
 
-def start(update: Update, context: CallbackContext) -> None:
-    chat_id = str(update.message.chat_id)
-    if "api" not in context.user_data:
-        update.message.reply_text(
-            'Привет, я мастер по бровям Алина, чем я могу Вам помочь?')
-        return
 
-def main() -> None:
-    token = Token().get()
-    updater = Updater(token)
-    updater.dispatcher.add_handler(CommandHandler("start", start))
-    updater.start_polling()
-    updater.idle()
+token = Token().get()
+bot = telebot.TeleBot(token)
 
 
-if __name__ == '__main__':
-    main()
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    mesg = bot.send_message(message.chat.id,'Привет, {0.first_name}! Для дальнейшей работы с ботом сообщите ваш ник-нейм в instagram ⬇️'.format(message.from_user))
+    bot.register_next_step_handler(mesg,test)
+    
+def test(message):
+    d = {'user_name': message.from_user.username, 'chat_id': message.from_user.id, 'instagram': message.text}
+    
+    markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+    item1 = types.KeyboardButton('Прайс 💵')
+    item2 = types.KeyboardButton('Расписание 🕐')
+
+    markup.add(item1, item2)
+
+    bot.send_message(message.chat.id, 'Спасибо, {0.first_name}! Чем я могу помочь?'.format(message.from_user), reply_markup = markup)
+
+@bot.message_handler(content_types=['text'])
+def bot_message(message):
+    if message.chat.type == 'private':
+        if message.text == 'Прайс 💵':
+            bot.send_message(message.chat.id, 'Долговременная укладка - 50р. Коррекция бровей - 35р. Перманент бровей - 300р.')
+        elif message.text == 'Расписание 🕐':
+            markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+            item3 = types.KeyboardButton('Август')
+            item4 = types.KeyboardButton('Сентябрь')
+            
+            markup.add(item3, item4)
+
+            bot.send_message(message.chat.id, 'На какой месяц желаете записаться?')
+
+bot.infinity_polling()
