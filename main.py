781517abfd471f85ec8c5eceab2d3ace0f0101ff
TemKaa1337 <temkaaw0wpro@gmail.com>
2022-08-25 17:41:18 +0300

    add text templates

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import telebot
-from telebot import types
 from src.Token import Token
 
 token = Token().get()
@@ -13,9 +12,9 @@ def welcome(message):
 def test(message):
     d = {'user_name': message.from_user.username, 'chat_id': message.from_user.id, 'instagram': message.text}
     
-    markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-    item1 = types.KeyboardButton('Прайс 💵')
-    item2 = types.KeyboardButton('Расписание 🕐')
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard = True)
+    item1 = telebot.types.KeyboardButton('Прайс 💵')
+    item2 = telebot.types.KeyboardButton('Расписание 🕐')
     markup.add(item1, item2)
 
     bot.send_message(message.chat.id, 'Спасибо, {0.first_name}! Чем я могу помочь?'.format(message.from_user), reply_markup = markup)
@@ -25,16 +24,16 @@ def bot_message(message):
     if message.chat.type == 'private':
         if message.text == 'Прайс 💵':
             bot.send_message(message.chat.id, 'Перманенентный макияж бровей- *250р.* \nДолговременная укладка с окрашиванием и коррекцией - *50р.* \nДолговременная укладка без окрашивания - *30р.* \nОкрашивание и коррекция - *30р.* \nОкрашивание - *20р.* \nКоррекция (пинцет\воск) - *15р.* \nДепиляция верхней губы - *5р.*', parse_mode= "Markdown")
-            markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-            item1 = types.KeyboardButton('Август')
-            item2 = types.KeyboardButton('Сентябрь')
+            markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard = True)
+            item1 = telebot.types.KeyboardButton('Август')
+            item2 = telebot.types.KeyboardButton('Сентябрь')
             markup.add(item1, item2)
             bot.send_message(message.chat.id, 'Желаете записаться?' , reply_markup = markup)
 
         elif message.text == 'Расписание 🕐':
-            markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
-            item1 = types.KeyboardButton('Август')
-            item2 = types.KeyboardButton('Сентябрь')
+            markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard = True)
+            item1 = telebot.types.KeyboardButton('Август')
+            item2 = telebot.types.KeyboardButton('Сентябрь')
             markup.add(item1, item2)
 
             bot.send_message(message.chat.id, 'На какой месяц желаете записаться?' , reply_markup = markup)
