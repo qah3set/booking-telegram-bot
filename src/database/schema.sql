410afe1d4711dbc23d33cfb6c40fb5f6087c97f5
TemKaa1337 <temkaaw0wpro@gmail.com>
2022-08-25 17:19:05 +0300

    add schedule table

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS schedule;
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
@@ -17,4 +18,11 @@ CREATE TABLE bookings (
     service VARCHAR NOT NULL,
     price NUMERIC(5, 2) NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE schedule (
+    id SERIAL PRIMARY KEY,
+    year INTEGER NOT NULL,
+    month INTEGER NOT NULL,
+    time JSON NOT NULL
 );
